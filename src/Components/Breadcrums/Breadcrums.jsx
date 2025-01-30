300f0abeadcd5554dd42cb3a1100b7c85f33d937
muhammadalifalhusain <muhalifalhusain@gmail.com>
2025-01-30 21:24:27 +0700

    fix route app

---
+++ src/Components/Breadcrums/Breadcrums.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import './Breadcrums.css'
+const Breadcrums = (props) => {
+    const {product} = props;
+  return (
+    <div className='breadcrum'>
+      Home
+    </div>
+  )
+}
+
+export default Breadcrums
